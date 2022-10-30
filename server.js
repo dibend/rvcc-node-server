0f767c4756daaf87fe5b74f37cc7f1b9ba95a501
David DiBenedetto <david@njweb.solutions>
2022-10-30 15:40:34 -0400

    Log directory name and file name
    Log directory and file names with __filename and __dirname

--- server.js
+++ server.js
@@ -1,14 +1,2 @@
-const http = require('http');
-
-const hostname = process.env.HOSTNAME || 'localhost';
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello World');
-});
-
-server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-});
+console.log(__filename);
+console.log(__dirname);
