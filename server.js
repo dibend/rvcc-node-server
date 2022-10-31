8b8ef72bd38c5e285f7395172b3986c9d4633479
David DiBenedetto <david@njweb.solutions>
2022-10-30 22:04:00 -0400

    404 status code
    Return 404 status code for paths besides / and /about

--- server.js
+++ server.js
@@ -4,19 +4,21 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
 
   let path = './';
   switch (req.url) {
     case '/':
       path += 'index.html';
+      res.statusCode = 200;
       break;
     case '/about':
       path += 'about.html';
+      res.statusCode = 200;
       break;
     default:
       path += '404.html';
+      res.statusCode = 404;
       break;
   }
   fs.readFile(path, (err, data) => {
