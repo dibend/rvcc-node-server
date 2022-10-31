6cb65565e8d2e596beb1ee81a546171bdd156626
David DiBenedetto <david@njweb.solutions>
2022-10-30 22:06:42 -0400

    Redirect unknown paths to /
    Set header location to redirect and status code 301

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const server = http.createServer((req, res) => {
       res.statusCode = 200;
       break;
     default:
-      path += '404.html';
-      res.statusCode = 404;
+      res.setHeader('Location', '/');
+      res.statusCode = 301;
       break;
   }
   fs.readFile(path, (err, data) => {
